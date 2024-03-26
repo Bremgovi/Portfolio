2e730b3dff9e576841285880e88f596609f343d4
Bremgovi <bremgovidev@gmail.com>
2024-03-26 01:11:53 -0600

    Changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/portfolio",
-    distDir: "build",
-    output: "export",
     reactStrictMode: false,
     images: {
       domains: ["localhost", "127.0.0.1"],
